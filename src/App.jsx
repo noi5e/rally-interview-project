c0cdcdd0eca99f2b70944496514d83eea8ac40c6
Will Gutierrez <will.gutierrez@gmail.com>
2022-09-23 15:48:08 -0700

    delete vite boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import BillsList from "./BillsList";
+import data from "./assets/bills.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Civix</h1>
+      <h2>Bill Tracker</h2>
+      <BillsList bills={data.bills} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
