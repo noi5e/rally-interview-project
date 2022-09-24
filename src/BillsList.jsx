39ddd86bbeaf7c41ed31c383cd70d2aad6ff5295
Will Gutierrez <will.gutierrez@gmail.com>
2022-09-24 14:28:51 -0700

    add persistance to localStorage

--- src/BillsList.jsx
+++ src/BillsList.jsx
@@ -6,18 +6,46 @@ function BillsList(props) {
 
   // run once on component load
   useEffect(() => {
-    setBills(props.bills);
+    const storedVotes = JSON.parse(localStorage.getItem("votes"));
+
+    // deep copy
+    const bills = JSON.parse(JSON.stringify(props.bills));
+
+    if (storedVotes) {
+      storedVotes.forEach((vote) => {
+        const bill = bills.find(
+          (bill) => vote.type === bill.type && vote.number === bill.number
+        );
+
+        if (bill) {
+          bill.isSupported = vote.isSupported;
+        }
+      });
+    }
+    setBills(bills);
   }, []);
 
   const handleClick = (type, number, isSupported) => {
     // deep copy bills array
     const newBills = JSON.parse(JSON.stringify(bills));
 
-    for (let i = 0; i < newBills.length; i++) {
-      if (newBills[i].type === type && newBills[i].number === number) {
-        newBills[i].isSupported = isSupported;
-      }
-    }
+    const updatedBill = newBills.find(
+      (bill) => bill.type === type && bill.number === number
+    );
+    updatedBill.isSupported = isSupported;
+
+    // update localStorage so that votes persist
+    const votes = localStorage.getItem("votes")
+      ? JSON.parse(localStorage.getItem("votes"))
+      : [];
+
+    votes.push({
+      type,
+      number,
+      isSupported,
+    });
+
+    localStorage.setItem("votes", JSON.stringify(votes));
 
     setBills(newBills);
   };
